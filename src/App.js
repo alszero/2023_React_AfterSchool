c4e1b37b3cb6a6526052b7967effdaeccead44dc
alszero <alszero05@gmail.com>
2023-04-18 19:39:45 +0900

    start01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [제목1, b1] = useState('찬실은복도많지');
+  let [제목2, b2] = useState('내서랍속에 행복');
+  let [제목3, b3] = useState('컴온컴온');
+  // document.querySelectorAll('h4')[0].innerHTML = post;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color : 'yellow', fontSize:'22px'}}>힐링영화</h4>
+      </div>
+      <div className="list">
+        <h4>{제목1}</h4>
+        <p>4월 18일</p>
+      </div>
+      <div className="list">
+        <h4>{제목2}</h4>
+        <p>4월 18일</p>
+      </div>
+      <div className="list">
+        <h4>{제목3}</h4>
+        <p>4월 18일</p>
+      </div>
     </div>
   );
 }
